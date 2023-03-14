3e4def7401e598fc756e153598799751e5dc79ee
m1ngmy0ng <49314605+sj1225@users.noreply.github.com>
2023-03-14 13:03:23 +0900

    Initial commit

---
+++ src/main/java/com/asteroid/voardapp/VoardAppApplication.java
@@ -0,0 +1,13 @@
+package com.asteroid.voardapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoardAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoardAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asteroid/voardapp/VoardAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asteroid.voardapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoardAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
