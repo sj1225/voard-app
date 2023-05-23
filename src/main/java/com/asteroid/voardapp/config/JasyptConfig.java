421161d2826a04403e4f1054d1b984cc42a704e0
m1ngmy0ng <49314605+sj1225@users.noreply.github.com>
2023-05-23 23:06:47 +0900

    config 패키지 생성 및 파일이동

--- src/main/java/com/asteroid/voardapp/config/JasyptConfig.java
+++ src/main/java/com/asteroid/voardapp/config/JasyptConfig.java
@@ -1,4 +1,4 @@
-package com.asteroid.voardapp;
+package com.asteroid.voardapp.config;
 
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
--- src/main/java/com/asteroid/voardapp/config/WebMvcConfig.java
+++ src/main/java/com/asteroid/voardapp/config/WebMvcConfig.java
@@ -1,4 +1,4 @@
-package com.asteroid.voardapp;
+package com.asteroid.voardapp.config;
 
 import java.io.IOException;
 
