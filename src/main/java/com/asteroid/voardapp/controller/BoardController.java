0527398cfd1d5074f640ceaa974f5a49aa4791c9
m1ngmy0ng <49314605+sj1225@users.noreply.github.com>
2023-06-09 20:08:25 +0900

    BoardController 수정
    왜 BoardMapper을 쓰고있었지... BoardServiceImpl로 수정함 변수명도 수정함

--- src/main/java/com/asteroid/voardapp/controller/BoardController.java
+++ src/main/java/com/asteroid/voardapp/controller/BoardController.java
@@ -1,54 +1,53 @@
 package com.asteroid.voardapp.controller;
 
-import com.asteroid.voardapp.mapper.BoardMapper;
 import com.asteroid.voardapp.model.Board;
+import com.asteroid.voardapp.service.BoardServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class BoardController {
     @Autowired
-    BoardMapper boardMapper;
+    BoardServiceImpl boardService;
 
     @GetMapping("/api/v1/board")
     public List<Board> getBoardList() {
-        List<Board> brd = boardMapper.getBoardList();
+        List<Board> brd = boardService.getBoardList();
         return brd;
     }
 
     @GetMapping("/api/v1/board/search")
     public List<Board> getBoardListSearch(
             @RequestParam String search_word) {
-        List<Board> brd = boardMapper.getBoardListSearch(search_word);
+        List<Board> brd = boardService.getBoardListSearch(search_word);
         return brd;
     }
 
     @GetMapping("/api/v1/board/{board_no}")
     public List<Board> getAllBoardList(@PathVariable(name = "board_no") Integer board_no) {
-        List<Board> brd = boardMapper.getBoardInfo(board_no);
+        List<Board> brd = boardService.getBoardInfo(board_no);
         return brd;
     }
 
     @PostMapping("/api/v1/board")
     public void insertBoardList(
             @RequestBody Board brdVo) {
-        boardMapper.insertBoardInfo(brdVo.getBoard_title(), brdVo.getBoard_user_id(), brdVo.getBoard_content());
+        boardService.insertBoardInfo(brdVo.getBoard_title(), brdVo.getBoard_user_id(), brdVo.getBoard_content());
     }
 
     @PatchMapping("/api/v1/board")
     public void updateBoardList(
             @RequestBody Board brdVo) {
-        boardMapper.updateBoardInfo(brdVo.getBoard_no(), brdVo.getBoard_title(), brdVo.getBoard_user_id(), brdVo.getBoard_content());
+        boardService.updateBoardInfo(brdVo.getBoard_no(), brdVo.getBoard_title(), brdVo.getBoard_user_id(), brdVo.getBoard_content());
     }
 
     @DeleteMapping("/api/v1/board")
     public void deleteBoardList(
             @RequestParam Integer board_no, @RequestParam String board_user_id) {
-        boardMapper.deleteBoardInfo(board_no, board_user_id);
+        boardService.deleteBoardInfo(board_no, board_user_id);
     }
 }
